4955c54bdd6589ab5badfa54ef6b8497670dd123
aishw <aishw@DESKTOP-O67ULCG>
2017-08-22 14:43:50 -0400

    initial code

---
+++ referencevariable/src/Ref.java
@@ -0,0 +1,16 @@
+class Student
+{
+int number;
+String name;
+}
+public class Ref {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Student s1 = new Student();
+s1.number = 123;
+s1.name = "Aish";
+System.out.println(s1.number+" "+s1.name);
+	}
+
+}
